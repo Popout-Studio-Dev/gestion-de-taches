c4fbc5fc6042714203ea4ff79a0922c67d7d44a0
KamgaBrayan <brayanarmel@gmail.com>
2025-03-07 21:39:27 +0100

    modification du nom du fichier contenant les interfaces

--- src/interfaces/task.tsx
+++ src/interfaces/task.tsx
@@ -3,34 +3,34 @@ export enum TaskStatus {
     WAITING = 'waiting',
     ONGOING = 'ongoing',
     COMPLETED = 'completed'
-  }
-  
-  // Interface principale pour une tâche
-  export interface Task {
+}
+
+// Interface principale pour une tâche
+export interface Task {
     id: string;
     title: string;
     description?: string;
     status: TaskStatus;
     dueDate?: string; // Date d'échéance au format
-    createdAt: string; 
-    updatedAt: string; 
-    startedAt?: string; 
-    completedAt?: string; 
-  }
-  
-  
-  export interface TaskMetadata {
+    createdAt: string;
+    updatedAt: string;
+    startedAt?: string;
+    completedAt?: string;
+}
+
+
+export interface TaskMetadata {
     totalTasks: number;
     waitingTasks: number;
     ongoingTasks: number;
     completedTasks: number;
-    overdueTasks: number; 
-  }
-  
-  
-  export interface TaskUpdate {
+    overdueTasks: number;
+}
+
+
+export interface TaskUpdate {
     title?: string;
-    description?: string | null; 
+    description?: string | null;
     status?: TaskStatus;
-    dueDate?: string | null; 
-  }
+    dueDate?: string | null;
+}
